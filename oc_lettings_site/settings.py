e1d326ce5ca96769db827283bb6ddbc0f9201961
MekkiZ <jmekki@gmail.com>
2022-12-20 16:20:02 +0100

    deploy for heroku

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -16,6 +17,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+django_heroku.settings(locals())
 
 # Application definition
 
