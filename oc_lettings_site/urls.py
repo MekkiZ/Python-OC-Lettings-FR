21a3862b62ab80238f712e79bf7b0f5b6ee72ab5
MekkiZ <jmekki@gmail.com>
2022-12-21 14:38:15 +0100

    test de deployement

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,10 +3,16 @@ from django.urls import path, include
 
 from lettings import views as view_letting
 
+
+def trigger_error(request):
+    return 1 / 0
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view_letting.home, name='index'),
     path('', include('lettings.urls')),
     path('', include('profiles.urls')),
+    path('sentry-debug/', trigger_error),
 
 ]
